31bb836545a0bc161efe7f0c1c3c3ae14b604876
Adnan Quazi <quaziadnan12352@gmail.com>
2024-09-30 20:02:57 +0530

    fc

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://menu-1sn8qqowf-adnan-quazis-projects.vercel.app",
     "https://zooptiq.vercel.app",
     "https://www.zooptick.com",
     "https://www.zooptick.in",
